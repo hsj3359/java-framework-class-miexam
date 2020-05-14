df2f472e6be02b59b1679e2614199e7c302aa590
hsj3359 <37091550+hsj3359@users.noreply.github.com>
2020-05-14 15:41:28 +0900

    과제 13

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -2,14 +2,12 @@ package kr.ac.jejunu.userdao;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
-
 import java.sql.Driver;
 
-@Configuration
 public class DaoFactory {
     @Value("${db.classname}")
     private String className;
@@ -26,8 +24,8 @@ public class DaoFactory {
         return new UserDao(jdbcContext());
     }
     @Bean
-    public JdbcContext jdbcContext(){
-        return new JdbcContext(dataSource());
+    public JdbcTemplate jdbcContext(){
+        return new JdbcTemplate(dataSource());
     }
     @Bean
     public DataSource dataSource(){
